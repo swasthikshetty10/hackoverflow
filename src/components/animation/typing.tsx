97e33e435ba85a566d20fe5021dbe057d0d1f171
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-03-18 06:24:40 +0530

    feat: modal

--- src/components/animation/typing.tsx
+++ src/components/animation/typing.tsx
@@ -4,7 +4,7 @@ export default function Typing() {
   return (
     <TypeAnimation
       cursor={true}
-      sequence={["Jab We Meet", 3000, "जब हम मिले", 3000, "ಜಬ್ ವಿ ಮೆಟ್", 3000]}
+      sequence={["Jab We Meet", 3000, "जब हम मिले", 3000, "お目にかかった際に", 3000]}
       wrapper="h2"
       className="gradient-text text-3xl font-semibold sm:text-3xl lg:text-3xl xl:text-5xl 2xl:text-6xl"
       repeat={Infinity}
